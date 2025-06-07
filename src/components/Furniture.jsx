e693492ca157ff087f23d50aef85a8358732d0ec
rinklepoonia <rinklepoonia889@gmail.com>
2025-06-07 16:27:40 +0530

    helper data

--- src/components/Furniture.jsx
+++ src/components/Furniture.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { OPTIONS_DATA_LIST } from "@/utils/helper";
+import { FURNITURE_IMAGES_DATA_LIST, OPTIONS_DATA_LIST } from "@/utils/helper";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Image from "next/image";
@@ -7,40 +7,7 @@ import React, { useLayoutEffect, useState } from "react";
 
 gsap.registerPlugin(ScrollTrigger);
 
-const imageGroups = [
-    [
-        { src: "/assets/images/webp/white-window.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0 h-[438px]", imageHeight: "h-[438px]", alt: "white-window" },
-        { src: "/assets/images/webp/study-table.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%] h-[325px]", imageHeight: "h-[325px]", alt: "study-table" },
-        { src: "/assets/images/webp/white-cahir.webp", width: 161, height: 212, className: "w-[12%] mt-[4%] h-[212px]", imageHeight: "h-[212px]", alt: "white-chair" },
-        { src: "/assets/images/webp/single-chair.webp", width: 420, height: 551, className: "w-[31%] mt-[24%] max-h-[551px]", imageHeight: "max-h-[551px]", alt: "single-chair" },
-    ],
-    [
-        { src: "/assets/images/webp/white-masrum.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "white-masrum" },
-        { src: "/assets/images/webp/white-lalten.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "white-lalten" },
-        { src: "/assets/images/webp/night-view.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "night-view" },
-        { src: "/assets/images/webp/white-big-bulb.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "white-big-bulb" },
-    ],
-    [
-        { src: "/assets/images/webp/library-bulb.webp", width: 420, height: 551, className: "w-[31%] mr-[6.25%] mt-[-13%]", imageHeight: "h-[551px]", alt: "library-bulb" },
-        { src: "/assets/images/webp/green-plant.webp", width: 334, height: 438, className: "w-[25%] mr-[25%] mt-[-8%]", imageHeight: "h-[438px]", alt: "green-plant" },
-    ],
-    [
-        { src: "/assets/images/webp/office-light.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "office-light" },
-        { src: "/assets/images/webp/two-chairs.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "two-chair" },
-        { src: "/assets/images/webp/gold-biskuts.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "gold-biskuts" },
-        { src: "/assets/images/webp/chair-table-laptop.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "chair-table-laptop" },
-    ],
-    [
-        { src: "/assets/images/webp/less-more.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "less-more" },
-        { src: "/assets/images/webp/tech-5.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "tech-5" },
-        { src: "/assets/images/webp/camera.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "camera" },
-        { src: "/assets/images/webp/digital-watch.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "digital-watch" },
-    ],
-    [
-        { src: "/assets/images/webp/cycle.webp", width: 420, height: 551, className: "w-[31%] mr-[6.25%] mt-[-13%]", imageHeight: "h-[551px]", alt: "cycle" },
-        { src: "/assets/images/webp/bony.webp", width: 334, height: 438, className: "w-[25%] mr-5 mt-[-8%]", imageHeight: "h-[438px]", alt: "bony" },
-    ],
-];
+
 
 const Furniture = () => {
     const [bgColor, setBgColor] = useState('#fff');
@@ -145,7 +112,7 @@ const Furniture = () => {
                         {displayText}
                     </p>
                 </div>
-                {imageGroups.map((group, index) => (
+                {FURNITURE_IMAGES_DATA_LIST.map((group, index) => (
                     <div
                         key={index}
                         className={`flex flex-row justify-start relative z-2 px-10  `}
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -54,3 +54,37 @@ export const FOOTER_DATA_LIST = [
         ],
     },
 ];
+export const FURNITURE_IMAGES_DATA_LIST = [
+    [
+        { src: "/assets/images/webp/white-window.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0 h-[438px]", imageHeight: "h-[438px]", alt: "white-window" },
+        { src: "/assets/images/webp/study-table.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%] h-[325px]", imageHeight: "h-[325px]", alt: "study-table" },
+        { src: "/assets/images/webp/white-cahir.webp", width: 161, height: 212, className: "w-[12%] mt-[4%] h-[212px]", imageHeight: "h-[212px]", alt: "white-chair" },
+        { src: "/assets/images/webp/single-chair.webp", width: 420, height: 551, className: "w-[31%] mt-[24%] max-h-[551px]", imageHeight: "max-h-[551px]", alt: "single-chair" },
+    ],
+    [
+        { src: "/assets/images/webp/white-masrum.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "white-masrum" },
+        { src: "/assets/images/webp/white-lalten.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "white-lalten" },
+        { src: "/assets/images/webp/night-view.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "night-view" },
+        { src: "/assets/images/webp/white-big-bulb.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "white-big-bulb" },
+    ],
+    [
+        { src: "/assets/images/webp/library-bulb.webp", width: 420, height: 551, className: "w-[31%] mr-[6.25%] mt-[-13%]", imageHeight: "h-[551px]", alt: "library-bulb" },
+        { src: "/assets/images/webp/green-plant.webp", width: 334, height: 438, className: "w-[25%] mr-[25%] mt-[-8%]", imageHeight: "h-[438px]", alt: "green-plant" },
+    ],
+    [
+        { src: "/assets/images/webp/office-light.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "office-light" },
+        { src: "/assets/images/webp/two-chairs.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "two-chair" },
+        { src: "/assets/images/webp/gold-biskuts.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "gold-biskuts" },
+        { src: "/assets/images/webp/chair-table-laptop.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "chair-table-laptop" },
+    ],
+    [
+        { src: "/assets/images/webp/less-more.webp", width: 334, height: 438, className: "w-[24%] mr-[6.25%] mt-0", imageHeight: "h-[438px]", alt: "less-more" },
+        { src: "/assets/images/webp/tech-5.webp", width: 248, height: 325, className: "w-[19%] mr-[6.25%] mt-[20%]", imageHeight: "h-[325px]", alt: "tech-5" },
+        { src: "/assets/images/webp/camera.webp", width: 161, height: 212, className: "w-[12%] mt-[4%]", imageHeight: "h-[212px]", alt: "camera" },
+        { src: "/assets/images/webp/digital-watch.webp", width: 420, height: 551, className: "w-[31%] mt-[24%]", imageHeight: "h-[551px]", alt: "digital-watch" },
+    ],
+    [
+        { src: "/assets/images/webp/cycle.webp", width: 420, height: 551, className: "w-[31%] mr-[6.25%] mt-[-13%]", imageHeight: "h-[551px]", alt: "cycle" },
+        { src: "/assets/images/webp/bony.webp", width: 334, height: 438, className: "w-[25%] mr-5 mt-[-8%]", imageHeight: "h-[438px]", alt: "bony" },
+    ],
+];
