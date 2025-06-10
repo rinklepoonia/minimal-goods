29c5c978ee674e493646bbd4dd656de5370a70f7
rinklepoonia <rinklepoonia889@gmail.com>
2025-06-10 11:16:23 +0530

    zoomImg-animation done

--- src/components/ZoomImageSection.jsx
+++ src/components/ZoomImageSection.jsx
@@ -22,26 +22,15 @@ const ZoomImageSection = () => {
                 pinSpacing: true,
                 animation: tl,
             })
-            // tl.fromTo("#imgZoom ", {
-            //     borderRadius: "100rem",
-            //     width: "500px",
-            //     height: "500px",
-            // }, {
-            //     borderRadius: "0",
-            //     width: "100vw",
-            //     height: "100vh",
-
-            // }, "<"
-            // )
             tl.fromTo("#imgZoom",
                 {
-                    width: `${circleHeight - 200}px`,
-                    height: `${circleHeight - 200}px`,
+                    width: `${circleHeight - 250}px`,
+                    height: `${circleHeight - 250}px`,
                     borderRadius: "9999px",
                 },
                 {
                     width: `${circleWidth - 18}px`,
-                    height: `${circleHeight - 69}px`,
+                    height: `${circleHeight - 0}px`,
                     borderRadius: "0px",
                     duration: 1,
                 }, "<"
@@ -65,16 +54,9 @@ const ZoomImageSection = () => {
         return () => ctx.revert();
     }, [])
     return (
-        // <div id='zoomImgEffect' className='h-[100vh] bg-[#2E2A27] z-[2] px-10 flex justify-center items-center overflow-hidden relative'>
-        //     <div id='imgZoom' className='mx-auto overflow-hidden w-full h-full flex items-center justify-center'>
-        //         <Image width={1920} height={1024} className='object-cover mx-auto' src="/assets/images/webp/room.webp" alt='her-img' />
-        //     </div>
-        // </div>
-        <div className="relative z-[2] bg-[#2E2A27] ">
-            <div id='zoomImgEffect' className='flex justify-center zoomImageContainer'>
-                <div className="flex justify-center items-center pt-[69px] w-full">
-                    <div id='imgZoom' className="rounded-full overflow-hidden bg-no-repeat bg-center image-wrapper bg-[url('/assets/images/webp/room.webp')]"></div>
-                </div>
+        <div id='zoomImgEffect' className='h-[100vh] bg-[#2E2A27] z-[2] px-10 flex justify-center items-center overflow-hidden relative'>
+            <div id='imgZoom' className='mx-auto overflow-hidden w-full h-full flex items-center justify-center'>
+                <div id='imgZoom' className="rounded-full overflow-hidden bg-no-repeat  bg-center image-wrapper bg-[url('/assets/images/png/white-sofa.png')]"></div>
             </div>
         </div>
     )
