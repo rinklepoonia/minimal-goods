3ed196b6cd49da4b2535bda9f52cea05e7b9281c
rinklepoonia <rinklepoonia889@gmail.com>
2025-06-26 11:07:20 +0530

    lenis-global

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,23 @@
 import Footer from "@/components/common/Footer";
 import FooterBootom from "@/components/common/FooterBootom";
 import NavBar from "@/components/common/NavBar";
+import UseLenis from "@/components/common/UseLenis";
 import Discover from "@/components/Discover";
 import Furniture from "@/components/Furniture";
 import ManyOptions from "@/components/ManyOptions";
-import SectionTwo from "@/components/SectionTwo";
 import ZoomImageSection from "@/components/ZoomImageSection";
-import Image from "next/image";
 
 export default function Home() {
   return (
-
     <div className="overflow-x-clip">
+      <UseLenis />
       <NavBar />
       <Discover />
       <ZoomImageSection />
       <ManyOptions />
       <Furniture />
       <Footer />
-      <FooterBootom/>
+      <FooterBootom />
     </div>
-
   );
 }
